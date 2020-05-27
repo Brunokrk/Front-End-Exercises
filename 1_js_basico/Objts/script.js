d7c4fe1db60d8fe0bf7ad13fff8730462ea6848e
Brunokrk <45456467+Brunokrk@users.noreply.github.com>
2020-05-27 17:22:32 -0300

    finalizando js bsc

--- 1_js_basico/Objts/script.js
+++ 1_js_basico/Objts/script.js
@@ -1,11 +1,34 @@
-var aluno = {
-    nome: "João",
-    notas: [7.5, 7.0]
+/*
+function criarAluno(nome,n1, n2){
+    return {
+        nome:nome,
+        nota1:n1,
+        nota2:n2,
+        media: function(){
+            return(this.nota1 +this.nota2) / 2;
+        }
+    }
 }
-//var aluno = New Object({...});
+*/
+function aluno(nome, n1, n2){
+    this.nome = nome;
+    this.nota1 = n1;
+    this.nota2 = n2;
+    this.media = function (){
+        return (this.nota1 +this.nota2)/2;
+    }
 
-aluno.matricula = 12345;
-aluno["sobrenome"]="Oliveira"
-
-console.log(aluno)
-
+}
+/*
+var turma = [
+        criarAluno("Igor", 9, 6),
+        criarAluno("Joao",5, 6),
+        criarAluno("Marcela", 7, 3)
+]
+*/
+var a = new aluno("Igor", 8, 7);
+var a = new aluno("Joao", 8, 7);
+console.log(a)
+//turma.forEach( function(elemento){
+//    console.log(elemento);
+//})
