c6495619797881668ac9ade014d47aca79e33dff
Brunokrk <45456467+Brunokrk@users.noreply.github.com>
2020-10-20 19:41:37 -0300

    .

---
+++ exercicios-js/fundamentos/string.js
@@ -0,0 +1,7 @@
+console.log('ABCDE'.split('')) //Cria um array com o separador escolhido
+
+const teste = "abcdef"
+
+console.log(teste.charAt(3))
+console.log(teste.indexOf('e'))
+console.log(teste.substring(0,3))
